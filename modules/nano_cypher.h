7af494f830beb882d2c07b6cef51c66a059ac8fe
adriano.bonin <adriano.bonin@outlook.com>
2021-07-02 12:44:30 -0300

    Created nano encryption mode

---
+++ modules/nano_cypher.h
@@ -0,0 +1,13 @@
+//
+// Created by Adriano on 02/07/2021.
+//
+
+#ifndef CRYPTOCYPHER_NANO_CYPHER_H
+#define CRYPTOCYPHER_NANO_CYPHER_H
+
+#include <vector>
+using namespace std;
+
+vector<char>nano_encrypt(vector<char> source);
+
+#endif //CRYPTOCYPHER_NANO_CYPHER_H
