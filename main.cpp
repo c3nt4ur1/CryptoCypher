102d0227b318688c4a26818007fd0421a2dd5953
adriano.bonin <adriano.bonin@outlook.com>
2021-07-01 23:39:38 -0300

    Encryption and decryption with cesar's cypher created and tested

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Adriano on 01/07/2021.
+//
+
+/*
+ * While the encryption and decryption modules are been created, this shall be used for testing the modules only
+ *
+ * The UI and the practical applications will only be developed after the modules are fully operand
+ *
+ * Code mustn't remain in this function whereas the modules are under development
+ */
+
+#include <iostream>
+#include "modules/cesar_cypher.h"
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
