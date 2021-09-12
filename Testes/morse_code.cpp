bf540af16e6de6352de09313fb4e2ad9c5edfcc9
Adriano <adriano.bonin@outlook.com>
2021-09-12 13:29:46 -0300

    Created the morse code support files

---
+++ Testes/morse_code.cpp
@@ -0,0 +1,9 @@
+//
+// Created by adria on 12/09/2021.
+//
+
+#include "morse_code.h"
+
+string string_to_morse(string s){
+
+}
---
+++ Testes/morse_code.h
@@ -0,0 +1,12 @@
+//
+// Created by adria on 12/09/2021.
+//
+#include <iostream>
+using std::string;
+
+#ifndef TESTES_MORSE_CODE_H
+#define TESTES_MORSE_CODE_H
+
+string string_to_morse(string s);
+
+#endif
