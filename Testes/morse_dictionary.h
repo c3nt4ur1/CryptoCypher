e46c89a42a070d57e55307a118684256ca365ea5
Adriano <adriano.bonin@outlook.com>
2021-09-12 13:30:18 -0300

    Macros to be used for morse encryption and decryption

---
+++ Testes/morse_dictionary.h
@@ -0,0 +1,38 @@
+//
+// Created by adria on 12/09/2021.
+//
+
+#ifndef TESTES_MORSE_DICTIONARY_H
+#define TESTES_MORSE_DICTIONARY_H
+
+#define a_morse .-
+#define b_morse -...
+#define c_morse -.-.
+#define d_morse -..
+#define e_morse .
+#define f_morse ..-.
+#define g_morse --.
+#define h_morse ....
+#define i_morse ..
+#define j_morse .---
+#define k_morse -.-
+#define l_morse .-..
+#define m_morse --
+#define n_morse -.
+#define o_morse ---
+#define p_morse .--.
+#define q_morse --.-
+#define r_morse .-.
+#define s_morse ...
+#define t_morse -
+#define u_morse ..-
+#define v_morse ...-
+#define w_morse .--
+#define x_morse -..-
+#define y_morse -.--
+#define z_morse --..
+#define dot_morse .-.-.-
+#define !_morse -.-.--
+#define ?_morse ..--..
+
+#endif //TESTES_MORSE_DICTIONARY_H
